a3fed21a768fe63e65b9d7fd2e48b38478840876
Ian V <ianenrico1@gmail.com>
2020-09-22 08:24:14 -0400

    backgrounds set on all pages

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "tailwind.macro";
+
+
+const Wrapper = styled.div`
+  ${tw`bg-gray-900 h-screen`}
+`
+
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return <Wrapper>Menu</Wrapper>;
 };
 
 export default Menu;
--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "tailwind.macro";
+
+const Wrapper = styled.div`
+  ${tw`bg-gray-900 h-screen`}
+`;
 
 const OurStory = () => {
-  return <div>Our Story</div>;
+  return <Wrapper>Our Story</Wrapper>;
 };
 
 export default OurStory;
