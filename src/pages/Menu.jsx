32493417ccae0738882ba223516fae9ae4a360c2
Ian V <ianenrico1@gmail.com>
2020-09-22 12:46:28 -0400

    Page Background Images

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "tailwind.macro";
+import bread2 from "../images/bread2.jpg";
 
+const Wrapper = styled.main`
+  ${tw`relative flex justify-center items-center`}
+`;
 
-const Wrapper = styled.div`
-  ${tw`bg-gray-900 h-screen`}
-`
-
+const Image = styled.img`
+  ${tw`h-screen w-full object-cover`}
+`;
 
 const Menu = () => {
-  return <Wrapper>Menu</Wrapper>;
+  return (
+    <Wrapper>
+      <Image src={bread2} alt="Table of Bread" />
+    </Wrapper>
+  );
 };
 
 export default Menu;
--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "tailwind.macro";
+import breadBoard from "../images/bread-board.jpg";
 
-const Wrapper = styled.div`
-  ${tw`bg-gray-900 h-screen`}
+const Wrapper = styled.main`
+  ${tw`relative flex justify-center items-center`}
+`;
+
+const Image = styled.img`
+  ${tw`h-screen w-full object-cover`}
 `;
 
 const OurStory = () => {
-  return <Wrapper>Our Story</Wrapper>;
+  return (
+    <Wrapper>
+      <Image src={breadBoard} alt="Table of bread" />
+    </Wrapper>
+  );
 };
 
 export default OurStory;
